d44781b61d5e019870ee232ca0896187f0058515
marijnbent <marijnbent@gmail.com>
2015-08-27 18:33:21 +0200

    Aantal aanpassingen over de dag

--- admin/checkfirebase.js
+++ admin/checkfirebase.js
@@ -5,7 +5,7 @@ var singlePointRef;
 
 function init() {
 //Setting up connection with Firebase
-    var myDataRef = new Firebase('https://tunedrop.firebaseio.com/');
+    var myDataRef = new Firebase('https://intro1a.firebaseio.com/');
     var pointRef = myDataRef.child("points");
 
     pointRef.on("value", function (snapshot) {
---
+++ admin/foto/main.js
@@ -0,0 +1,21 @@
+init();
+
+var fireData;
+
+function init() {
+//Setting up connection with Firebase
+    var myDataRef = new Firebase('https://intro1d.firebaseio.com/');
+    var pointRef = myDataRef.child("points");
+
+    pointRef.on("value", function (snapshot) {
+        //All points from firebase
+        fireData = snapshot.val();
+        console.log(fireData);
+
+        $.each(fireData, function (i, contents) {
+            console.log(contents.photo);
+            $("#foto").append('<img src="' + contents.photo + '">');
+        });
+
+    });
+}
